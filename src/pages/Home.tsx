d4e8bb801358f6ff81f88a11eb6639d536a3fc22
unknown <tanmay.kaushik451@gmail.com>
2020-07-26 16:08:32 +0530

    loading spinner and wrapping up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,46 @@
-import React, { useEffect, useContext } from "react"
-import ShipmentContext from "../context/ShipmentContext"
-import ShipmentCounter from "../components/ShipmentCounter"
-import ItemsTable from "../components/ItemsTable"
-import StatusStepper from "../components/StatusStepper"
+import React, { useEffect, useContext } from "react";
+import ShipmentContext from "../context/ShipmentContext";
+import ShipmentCounter from "../components/ShipmentCounter";
+import ItemsTable from "../components/ItemsTable";
+import StatusStepper from "../components/StatusStepper";
 
-const Home : React.FC = ()=>{
+const Home: React.FC = () => {
+  const shipmentContext = useContext(ShipmentContext);
 
-    const shipmentContext = useContext(ShipmentContext)
+  const { getshipments, setdefaultfilter } = shipmentContext;
 
-    const {getshipments,setdefaultfilter} = shipmentContext
+  const { shipments } = shipmentContext.State;
 
-    const {shipments} = shipmentContext.State
+  useEffect(() => {
+    getshipments();
+    // eslint-disable-next-line
+  }, []);
 
-    useEffect(()=>{
-        getshipments()
-        // eslint-disable-next-line
-    },[])
+  useEffect(() => {
+    setdefaultfilter();
+  }, [shipments]);
 
-    useEffect(()=>{
-        setdefaultfilter()
-    },[shipments])
-    
-    return(
-        <div className="d-flex flex-column">
-            <ShipmentCounter/>
-            <div className="d-flex align-items-center">
-                <div className="stepper"><StatusStepper/></div>
-                <div className="tablediv"><ItemsTable/></div>
-            </div>
+  return (
+    <div className="d-flex flex-column justify-content-center">
+      {shipments.length !== 0 ? (
+        <ShipmentCounter />
+      ) : (
+        <div className="align-self-center mt-5">
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
         </div>
-    )
-}
+      )}
+      <div className="d-flex align-items-center">
+        <div className="stepper">
+          <StatusStepper />
+        </div>
+        <div className="tablediv">
+          <ItemsTable />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
